e56e64598c95e434226e90a00a3caa7c40ca1ef7
YY <ky040723@gmail.com>
2024-02-01 14:38:19 +0900

    사운드매니저

---
+++ NeoBomerMan_TeamProject/Assets/02.Scripts/SoundManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip BgmClip;
+    public float BgmVolume;
+    AudioSource BgmPlayer;
+
+    public AudioClip[] SfxClips;
+    public float SfxVolume;
+    public int channels;
+    AudioSource[] SfxPlayer;
+    int channelIndex;
+
+    public enum Sfx { Start, SetBomb, }
+
+    public static SoundManager instance;
+    private void Awake()
+    {
+        instance = this;
+        Init();
+    }
+
+   void Init()
+    {
+        GameObject bgmObject = new GameObject("BgmPlayer");
+        bgmObject.transform.parent = transform;
+        BgmPlayer = bgmObject.AddComponent<AudioSource>();
+        BgmPlayer.playOnAwake = false;
+        BgmPlayer.loop = true;
+        BgmPlayer.volume = BgmVolume;
+        BgmPlayer.clip = BgmClip;
+
+        GameObject SfxObject = new GameObject("SfxPlayer");
+        SfxObject.transform.parent = transform;
+        SfxPlayer = new AudioSource[channels];
+
+        for (int i = 0; i < SfxPlayer.Length; i++)
+        {
+            SfxPlayer[i] = SfxObject.AddComponent<AudioSource>();
+            SfxPlayer[i].playOnAwake = false;
+            SfxPlayer[i].volume = SfxVolume;
+        }
+    }
+
+    public void PlayBgm(bool isPlay)
+    {
+        if (isPlay)
+        {
+            BgmPlayer.Play();
+        }
+        else
+        {
+            BgmPlayer.Stop();
+        }
+    }
+
+    public void PlaySfx (Sfx sfx)
+    {
+        for (int i = 0; i < SfxPlayer.Length; i++)
+        {
+            int loopIndex = (i + channelIndex) % SfxPlayer.Length;
+
+            if (SfxPlayer[loopIndex].isPlaying)
+            {
+                continue;
+            }
+            channelIndex = loopIndex;
+            SfxPlayer[loopIndex].clip = SfxClips[(int)sfx];
+            SfxPlayer[loopIndex].Play();
+            break;
+        }   
+
+    }  
+}
