0d95b79aac3cf7618699ef23487b8e9b3565442d
YY <ky040723@gmail.com>
2024-02-05 18:18:27 +0900

    [김예은] 엔딩크레딧 구현

--- NeoBomerMan_TeamProject/Assets/02.Scripts/EndingText.cs
+++ NeoBomerMan_TeamProject/Assets/02.Scripts/EndingText.cs
@@ -2,36 +2,51 @@ using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 
 public class EndingText : MonoBehaviour
 {
     public Text[] TextList;
     public List<string> texts = new List<string>();
-    public float Speed = 20f;
+    public float Speed = 1f;
+    int textIndex = 0;
+    bool textLoop = true;
 
     void Start()
     {
         for (int i = 0; i < TextList.Length; i++)
         {
-            if (TextList.Length > 0 && texts.Count > 0)
-            {
-                TextList[0].text = texts[0];
-            }
-        }   
+            texts.Add(string.Empty);
+        }
+        
     }
+
     void Update()
     {
-        foreach (Text t in TextList)
+        if (textLoop)
         {
-            Vector2 currentPosition = t.transform.position;
-            Vector2 newPosition = currentPosition + Vector2.up * Speed * Time.deltaTime;
-            t.transform.position = newPosition;
-            
-            if (newPosition.y > 0)
+            foreach(Text t in TextList)
             {
-                newPosition.y = -100;
+                Vector2 currentPosition = t.transform.position;
+                Vector2 newPosition = currentPosition + Vector2.up * Speed * Time.deltaTime;
+                t.transform.position = newPosition;
+
+                if (t.gameObject.transform.localPosition.y > 300)
+                {
+                    t.gameObject.transform.localPosition = new Vector2(t.gameObject.transform.localPosition.x, -300);
+                    t.text = texts[textIndex];
+
+                    if (texts.Count - 1 > textIndex)
+                    {
+                        textIndex++;
+                    }
+                    else
+                    {
+                        textLoop = false;
+                    }
+                }
             }
         }
-    }  
+    }
 }
